59baba97cce642ecea41fc1b68b7033f7716c833
Bárbara Port <barbara.port@outlook.com>
2022-03-12 21:03:47 -0300

    build_query_sort_project method

--- mflix/db.py
+++ mflix/db.py
@@ -204,7 +204,11 @@ def build_query_sort_project(filters):
 
             # TODO: Text and Subfield Search
             # Construct a query that will search for the chosen genre.
-            query = {}
+            query = {
+                "genres": {
+                    "$in": filters["genres"]
+                }
+            }
 
     return query, sort, project
 
